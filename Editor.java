1c8b53bc47961d192d4e177affd68346865e5d15
spiderocta <muhammadahmed@tutanota.com>
2023-03-09 03:45:40 +0200

    Handling File Operations

--- Editor.java
+++ Editor.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Editor extends JFrame implements ActionListener {
     // editor components
@@ -12,6 +17,11 @@ public class Editor extends JFrame implements ActionListener {
     JSpinner fontSizeSpinner;
     JButton fontColorButton;
     JComboBox fontBox;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
     Editor(){
         this.setTitle("CodeClause_Editor");
         this.setSize(800,600);
@@ -39,7 +49,7 @@ public class Editor extends JFrame implements ActionListener {
         fontSizeSpinner.setValue(18);
         fontSizeSpinner.addChangeListener(e -> textArea.setFont(new Font(textArea.getFont().getFamily(),Font.PLAIN,(int) fontSizeSpinner.getValue())));
 
-        // colors button
+        // colors and fonts button
         fontColorButton = new JButton("Color");
         fontColorButton.addActionListener(this);
 
@@ -49,12 +59,31 @@ public class Editor extends JFrame implements ActionListener {
         fontBox.addActionListener(this);
         fontBox.setSelectedItem("Calibri");
 
+        // Menus
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+        // attach elements
+        this.setJMenuBar(menuBar);
         this.add(fontLabel);
         this.add(fontSizeSpinner);
         this.add(fontColorButton);
         this.add(fontBox);
         this.add(scrollPane);
         this.setVisible(true);
+
         //to center the window on lunch
         this.setLocationRelativeTo(null);
     }
@@ -70,6 +99,62 @@ public class Editor extends JFrame implements ActionListener {
 
         if(e.getSource()==fontBox) {
             textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN,textArea.getFont().getSize()));
+        }
+        if(e.getSource()==openItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()) {
+                        while(fileIn.hasNextLine()) {
+                            String line = fileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                finally {
+                    fileIn.close();
+                }
+            }
+        }
+        if(e.getSource()==saveItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                }
+                catch (FileNotFoundException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                finally {
+                    fileOut.close();
+                }
+            }
+        }
+        if(e.getSource()==exitItem) {
+            System.exit(0);
         }
     }
 }
