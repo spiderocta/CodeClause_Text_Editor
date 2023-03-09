f51bc4e7e2c33a7dfcda70e8a637e5d5d3ce66d3
spiderocta <muhammadahmed@tutanota.com>
2023-03-09 03:22:28 +0200

    basic text area

--- Editor.java
+++ Editor.java
@@ -4,12 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Editor extends JFrame implements ActionListener {
-
+    // editor components
+    JTextArea textArea;
     Editor(){
         this.setTitle("CodeClause_Editor");
         this.setSize(800,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(750, 550));
+        this .add(textArea);
         this.setVisible(true);
         //to center the window on lunch
         this.setLocationRelativeTo(null);
