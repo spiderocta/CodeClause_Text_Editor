d5ff7e9dc3b0a65ae64d5730b959465285c31024
spiderocta <muhammadahmed@tutanota.com>
2023-03-09 03:29:29 +0200

    scroll pane

--- Editor.java
+++ Editor.java
@@ -6,18 +6,28 @@ import java.awt.event.ActionListener;
 public class Editor extends JFrame implements ActionListener {
     // editor components
     JTextArea textArea;
+    JScrollPane scrollPane;
     Editor(){
         this.setTitle("CodeClause_Editor");
         this.setSize(800,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
+        //Text area Component
         textArea = new JTextArea();
         textArea.setPreferredSize(new Dimension(750, 550));
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
+        //default font
+        textArea.setFont(new Font("Calibri", Font.PLAIN, 18));
 
-        this .add(textArea);
+        //scroll pane component
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(750,550));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        
+        this.add(scrollPane);
         this.setVisible(true);
         //to center the window on lunch
         this.setLocationRelativeTo(null);
