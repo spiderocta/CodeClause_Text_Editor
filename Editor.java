c2974b136e431f9080b6bc1a2c0df9c4b8d450f6
spiderocta <muhammadahmed@tutanota.com>
2023-03-09 03:18:25 +0200

    Editor Main props

--- Editor.java
+++ Editor.java
@@ -1,5 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Editor extends JFrame {
+public class Editor extends JFrame implements ActionListener {
 
+    Editor(){
+        this.setTitle("CodeClause_Editor");
+        this.setSize(800,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
+        //to center the window on lunch
+        this.setLocationRelativeTo(null);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
